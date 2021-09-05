1344842a2ba076eb6e65fab78321f7d0cee80e76
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-09-05 12:19:08 +0100

    update(axios) trying to pull data for the past launches

--- controllers/launches.js
+++ controllers/launches.js
@@ -1,11 +1,34 @@
+const axios = require('axios');
+
 module.exports.index = (req, res) => {
     res.send("Made it to the Index")
 };
 
-module.exports.upcomingLaunch = (req, res) => {
-    res.send("Made it to the Upcoming Launch")
+module.exports.upcomingLaunch = async (req, res) => {
+    try {
+        const [response1, response2] = await axios.all([
+            axios.get('https://api.spacexdata.com/v3/launches/67'),
+            axios.get('https://api.spacexdata.com/v3/launches/next')
+        ]);
+        console.log(response1.data.mission_name);
+        console.log(response1.data.launch_year);
+
+        console.log(response2.data.mission_name);
+        console.log(response2.data.launch_year);
+        res.send("Chicken Dinner")
+    } catch (error) {
+        console.log(error.response.body);
+        res.send("We Have a Problem")
+    };
 };
 
-module.exports.previousLaunch = (req, res) => {
-    res.send("Made it to the Previous")
+module.exports.previousLaunch = async (req, res) => {
+    try {
+        const launchData = await axios.get('https://api.spacexdata.com/v3/launches')
+        res.render('launches/previous', { launchData })
+
+    } catch (error) {
+        console.log(error.body);
+        res.send("We Have a Problem")
+    }
 };
